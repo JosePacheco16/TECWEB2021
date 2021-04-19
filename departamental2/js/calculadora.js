b7035bf5ad1762d19af5353b93ba35299c151c20
Jose Pacheco <jose.pacheco.gtz16@gmail.com>
2021-04-19 15:53:27 -0500

    new

---
+++ departamental2/js/calculadora.js
@@ -0,0 +1,33 @@
+
+function sumaNumeros() {
+    var num1 = document.getElementById("numero1").value;
+    var num2 = document.getElementById("numero2").value;
+    var resultado = document.getElementById("resultado");
+
+    var suma = parseInt(num1) + parseInt(num2);
+
+    resultado.innerHTML="Resultado: " + suma;
+}
+
+function restaNumeros() {
+    var resta = document.getElementById("numero1").value - document.getElementById("numero2").value;
+    document.getElementById("resultado").innerHTML="Resultado: " + resta;
+}
+
+function multiplicaNumeros() {
+    var num1 = document.getElementById("numero1").value;
+    var num2 = document.getElementById("numero2").value;
+    var resultado = document.getElementById("resultado");
+
+    var multiplicacion = num1 * num2;
+    resultado.innerHTML = "Resultado: " + multiplicacion;
+}
+
+function divideNumeros() {
+    var num1 = document.getElementById("numero1").value;
+    var num2 = document.getElementById("numero2").value;
+    var resultado = document.getElementById("resultado");
+
+    var division = num1 / num2;
+    resultado.innerHTML = "Resultado: " + division;
+}
